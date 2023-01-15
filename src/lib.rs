dc701307b10a1da0a257ed70b39c1d058a0328fd
Benii <benii.alco@gmail.com>
2023-01-15 12:36:44 +0000

    Added function to parse a url-encoded name & email at the address '<application>/subscribe' data using Serde, and a test said funcion.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,30 @@
 use std::net::TcpListener;
 
-use axum::{http::StatusCode, routing::get, routing::IntoMakeService, Router, Server};
+use axum::{
+    extract::Form, http::StatusCode, routing::get, routing::post, routing::IntoMakeService, Router,
+    Server,
+};
 use hyper::server::conn::AddrIncoming;
+use serde::Deserialize;
 
 async fn health_check() -> StatusCode {
     StatusCode::OK
 }
 
+#[derive(Debug, Deserialize)]
+struct Subscription {
+    name: String,
+    email: String,
+}
+
+async fn subscribe(Form(_input): Form<Subscription>) -> StatusCode {
+    StatusCode::OK
+}
+
 pub fn run(listener: TcpListener) -> hyper::Result<Server<AddrIncoming, IntoMakeService<Router>>> {
-    let app = Router::new().route("/health_check", get(health_check));
+    let app = Router::new()
+        .route("/health_check", get(health_check))
+        .route("/subscribe", post(subscribe));
 
     let server = Server::from_tcp(listener)?.serve(app.into_make_service());
 

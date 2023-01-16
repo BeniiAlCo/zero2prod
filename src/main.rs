b63c77903cf20df487162d1dab48adfd45c8eb9b
Benii <benii.alco@gmail.com>
2023-01-16 09:43:20 +0000

    Added basic sketch of config file, and updated main to use it.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 use std::net::TcpListener;
-use zero2prod::startup;
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> hyper::Result<()> {
-    startup::run(TcpListener::bind("127.0.0.1:8000").expect("Failed to bind port 8000"))?.await
+    let configuration = get_configuration().expect("Failed to read configuration.");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener =
+        TcpListener::bind(address).unwrap_or_else(|port| panic!("Failed to bind to port {port}"));
+
+    run(listener)?.await
 }

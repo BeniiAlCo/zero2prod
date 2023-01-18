7fb880bba4e91cb1eec835abc4c4fb5fe3267b06
Benii <benii.alco@gmail.com>
2023-01-18 11:53:50 +0000

    Initial implementation of basic logging.

--- src/startup.rs
+++ src/startup.rs
@@ -1,20 +1,34 @@
-use axum::{routing::get, routing::post, routing::IntoMakeService, Router, Server};
-use deadpool_postgres::Pool;
-use hyper::server::conn::AddrIncoming;
+use axum::{routing, routing::get, routing::post};
+use hyper::server::conn;
 use std::net::TcpListener;
+use tower_http::trace;
+use tracing::Level;
 
 use crate::routes::{health_check, subscribe};
 
 pub fn run(
     listener: TcpListener,
-    connection: Pool,
-) -> hyper::Result<Server<AddrIncoming, IntoMakeService<Router>>> {
-    let app = Router::new()
+    connection: deadpool_postgres::Pool,
+) -> hyper::Result<axum::Server<conn::AddrIncoming, routing::IntoMakeService<axum::Router>>> {
+    tracing_subscriber::fmt().init();
+
+    let app = axum::Router::new()
         .route("/health_check", get(health_check))
         .route("/subscribe", post(subscribe))
-        .with_state(connection);
+        .with_state(connection)
+        .layer(
+            trace::TraceLayer::new_for_http()
+                .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
+                .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
+        );
 
-    let server = Server::from_tcp(listener)?.serve(app.into_make_service());
+    tracing::info!(
+        "listening on {:?}",
+        listener
+            .local_addr()
+            .expect("Error extracting local address from TCP listener.")
+    );
+    let server = axum::Server::from_tcp(listener)?.serve(app.into_make_service());
 
     Ok(server)
 }

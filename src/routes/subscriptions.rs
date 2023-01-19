8773a9a832332d330c465bab4b62c753431df543
Benii <benii.alco@gmail.com>
2023-01-19 18:22:07 +0000

    Formatting.

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -17,61 +17,66 @@ pub struct FormData {
     name = "Adding a new subscriber",
     skip_all,
     fields(
-        subscriber_email = %form.email, 
-        subscriber_name = %form.name), 
+        subscriber_email = %form.email,
+        subscriber_name = %form.name),
     level = "info"
 )]
 pub async fn subscribe(
-    State(pool): State<
-        bb8::Pool<bb8_postgres::PostgresConnectionManager<tokio_postgres::NoTls>>,
-    >,
+    State(pool): State<bb8::Pool<bb8_postgres::PostgresConnectionManager<tokio_postgres::NoTls>>>,
     Form(form): Form<FormData>,
 ) -> StatusCode {
     match get_connection(&pool).await {
-        Ok(connection) => {
-            match insert_subscriber(&connection, &form).await {
-                Ok(_) => StatusCode::OK,
-                Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
-            }
-        }
-        Err(_) => StatusCode::INTERNAL_SERVER_ERROR
+        Ok(connection) => match insert_subscriber(&connection, &form).await {
+            Ok(_) => StatusCode::OK,
+            Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        },
+        Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
     }
 }
 
-#[instrument(
-    name = "Saving new subscriber details in the database",
-    skip_all,
-)]
+#[instrument(name = "Saving new subscriber details in the database", skip_all)]
 async fn insert_subscriber(
-    connection: &bb8::PooledConnection<'_, bb8_postgres::PostgresConnectionManager<tokio_postgres::NoTls>>,
+    connection: &bb8::PooledConnection<
+        '_,
+        bb8_postgres::PostgresConnectionManager<tokio_postgres::NoTls>,
+    >,
     form: &FormData,
 ) -> Result<(), tokio_postgres::Error> {
-connection.execute(
-        "INSERT INTO Subscriptions (id, email, name, subscribed_at) 
+    connection
+        .execute(
+            "INSERT INTO Subscriptions (id, email, name, subscribed_at) 
     VALUES ($1, $2, $3, $4)",
-        &[
-            &Uuid::new_v4(),
-            &form.email,
-            &form.name,
-            &OffsetDateTime::now_utc(),
-        ],
-    )
-    .await.map_err(|e| {
-        tracing::error!("Failed to execute query: {:?}", e);
-        e
-    })?;
+            &[
+                &Uuid::new_v4(),
+                &form.email,
+                &form.name,
+                &OffsetDateTime::now_utc(),
+            ],
+        )
+        .await
+        .map_err(|e| {
+            tracing::error!("Failed to execute query: {:?}", e);
+            e
+        })?;
 
-    Ok(())           
+    Ok(())
 }
 
 #[instrument(
     name = "Establishing new connection to the database"
     skip_all,
 )]
-async fn get_connection(pool: &bb8::Pool<bb8_postgres::PostgresConnectionManager<tokio_postgres::NoTls>>) 
-    -> Result<bb8::PooledConnection<bb8_postgres::PostgresConnectionManager<tokio_postgres::NoTls>>, bb8::RunError<tokio_postgres::Error>> {
+async fn get_connection(
+    pool: &bb8::Pool<bb8_postgres::PostgresConnectionManager<tokio_postgres::NoTls>>,
+) -> Result<
+    bb8::PooledConnection<bb8_postgres::PostgresConnectionManager<tokio_postgres::NoTls>>,
+    bb8::RunError<tokio_postgres::Error>,
+> {
     pool.get().await.map_err(|e| {
-        tracing::error!("Failed to get connection to database from connection pool: {:?}", e);
+        tracing::error!(
+            "Failed to get connection to database from connection pool: {:?}",
+            e
+        );
         e
     })
 }
